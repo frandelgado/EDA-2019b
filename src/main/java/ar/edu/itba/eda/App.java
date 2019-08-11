5c0cd9e235c2f7324d416b6bcbfaacb1a766e682
fdelgado96 <fdelgado@itba.edu.ar>
2019-08-11 18:59:30 -0300

    Add initial project files

---
+++ src/main/java/ar/edu/itba/eda/App.java
@@ -0,0 +1,16 @@
+package ar.edu.itba.eda;
+
+import org.joda.time.DateTime;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Hello World!");
+        System.out.println(DateTime.now());
+    }
+}
